b9fc5f4c91ca6e2871f3fdfffa219541e7366f28
KarthikKurugodu <karthik.kurugodu@blend360.com>
2023-03-06 12:50:56 +0530

    changes to test.py

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 from my_library.utils import preprocessing as pp
 
 pps = pp.Preprocessing('maurices.csv')
-pps = pp.Preprocessing('segmentation_nonkroger_dataset_v3.csv')
-# print(pps.show_variable_types(pps.vyper('indiv_id_mcvp')))
+# pps = pp.Preprocessing('segmentation_nonkroger_dataset_v3.csv')
 a, B, nv, bv = pps.recoding(0.98, 0.1, 'indiv_id_mcvp', )
-# # a, B, nv, bv = pps.recoding(0.98, 0.1, 'person_id', )
+# a, B, nv, bv = pps.recoding(0.98, 0.1, 'person_id', )
 num_vars = [s + '_processed' for s in nv]
 bin_vars = [s + '_ind' for s in bv]
 pps.impute_na(num_vars, 'mean')
@@ -19,7 +18,7 @@ pps.tocsv('maurices_oc')
 from my_library.algorithms.k_means import k_means as km
 
 kms = km.Kmeans('maurices_oc_preprocessed.csv')
-# kms = km.Kmeans('segmentation_nonkroger_dataset_v3_preprocessed.csv')
+# kms = km.Kmeans('segmentation_nonkroger_dataset_v3_oc_preprocessed.csv')
 kms.Kmeans_elbow_plot(2, 10)
 kms.kmeans_minClusterSize(5, 10000)
 kms.kmeans_maxClusterSize(5, 10000)
