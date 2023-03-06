89581a2b4881724587c00e750108b8697d7e6867
KarthikKurugodu <karthik.kurugodu@blend360.com>
2023-03-06 13:33:38 +0530

    changes to test.py

--- test.py
+++ test.py
@@ -1,24 +1,20 @@
 from my_library.utils import preprocessing as pp
 
 pps = pp.Preprocessing('maurices.csv')
-# pps = pp.Preprocessing('segmentation_nonkroger_dataset_v3.csv')
 a, B, nv, bv = pps.recoding(0.98, 0.1, 'indiv_id_mcvp', )
-# a, B, nv, bv = pps.recoding(0.98, 0.1, 'person_id', )
 num_vars = [s + '_processed' for s in nv]
 bin_vars = [s + '_ind' for s in bv]
 pps.impute_na(num_vars, 'mean')
 pps.impute_na(bin_vars, 'mode')
 pps.correlation(num_vars, 0.8)
-pps.oc(6)  # comment for goodrx data
+pps.oc(6)
 pps.standardization()
 pps.print_shape()
 pps.tocsv('maurices_oc')
-# pps.tocsv('segmentation_nonkroger_dataset_v3_oc')
 # pip install ortools==9.3.10459
 from my_library.algorithms.k_means import k_means as km
 
 kms = km.Kmeans('maurices_oc_preprocessed.csv')
-# kms = km.Kmeans('segmentation_nonkroger_dataset_v3_oc_preprocessed.csv')
 kms.Kmeans_elbow_plot(2, 10)
 kms.kmeans_minClusterSize(5, 10000)
 kms.kmeans_maxClusterSize(5, 10000)
