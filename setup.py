7d1ba1ef5a2ee4a525f7ceb969d8e8de417ce897
KarthikKurugodu <karthik.kurugodu@blend360.com>
2023-06-15 12:47:39 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UVyper',
+    version='1.0',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    url='https://github.com/BLEND360/UVyper/tree/idc_dev1',
+    license='',
+    author='KarthikKurugodu',
+    author_email='',
+    description=''
+)
