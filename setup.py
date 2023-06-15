6e2b63aa8ee765c9214cd425a58f806b5fe311d7
KarthikKurugodu <karthik.kurugodu@blend360.com>
2023-06-15 11:25:10 +0530

    minor change to hierarchical documentation in README.md

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='UVyper',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/BLEND360/UVyper/tree/idc_dev1',
+    license='',
+    author='KarthikKurugodu',
+    author_email='',
+    description=''
+)
